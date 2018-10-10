c38bba99a41fce28b88fef06b88c095fbbe8b5b4
ceccs17d60 <jacobmathew711@gmail.com>
2018-10-10 20:46:40 +0530

    commit_message

---
+++ bubblesort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+int j,k,i,n,swap,l;
+printf("enter number of array elements\n");
+scanf("%d",&n);
+int a[n];
+printf("enter array elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(j=0;j<n-1;j++)
+{
+  for (k=0;k<n-j-1;k++)
+  {
+    if (a[k] > a[k+1])
+    {
+      swap       = a[k];
+      a[k]   = a[k+1];
+      a[k+1] = swap;
+    }
+  }
+}
+printf("Sorted array:\n");
+for(l=0;l<n;l++)
+{
+printf("%d\n", a[l]);
+}
+}
---
+++ insertionsort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+int j,k,i,n,temp,g,c;
+printf("enter array limit\n");
+scanf("%d",&n);
+int a[n];
+printf("enter array elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(k=1;k<n;k++)
+{
+temp = a[k];
+j=k-1;
+  while(temp<a[j] && j>=0)
+  {
+  a[j+1] = a[j];
+  --j;
+  }
+  a[j+1]=temp;
+}
+printf("sorted array:\n");
+for(c=0; c<n; c++)
+{
+printf("%d\n",a[c]);
+}
+}
